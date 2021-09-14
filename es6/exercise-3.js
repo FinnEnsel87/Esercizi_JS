4f306ee434fa7ea51374a17aa2745df69c55e93d
FinnEnsel87 <fgsantaniello@gmail.com>
2021-09-14 10:48:35 +0200

    feat: exercise 6-7-8-9

---
+++ es6/exercise-3.js
@@ -0,0 +1,26 @@
+function adultFilter(persons) {
+  const adults = [];
+  for (soggetto of persons)
+    if (soggetto.age >= 18)
+      adults.push (soggetto)
+  return adults
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+
+const adults = adultFilter(persons);
+
+console.log(persons);
+console.log(adults);
